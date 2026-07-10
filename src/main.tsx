3134034006476c09d9e53a64736f146ba9de8551
mxu88945-lgtm <mxu88945@gmail.com>
2026-07-10 19:54:14 +0800

    Remove conflicting navigation interceptor

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ import './homeShell.css'
 import { installChatDrawer } from './chatDrawer'
 import { installConversationDrawer } from './conversationDrawer'
 import { installHomeShell } from './homeShell'
-import { installNavigationFix } from './navigationFix'
 
 const APP_SCOPE = '/wewei-role-site/'
 let isReloading = false
@@ -102,7 +101,6 @@ observer.observe(document.documentElement, { childList: true, subtree: true })
 installChatDrawer()
 installConversationDrawer()
 installHomeShell()
-installNavigationFix()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
