dac85dde34e6c8a327e01a42302829f5b4d25e37
mxu88945-lgtm <mxu88945@gmail.com>
2026-08-15 16:01:00 +0800

    style imported character card bubbles

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import './runtime-enhancements.css'
 import './memoryApiEnhancements'
 
 const APP_SCOPE = '/wewei-role-site/'
-const SERVICE_WORKER_RELEASE = '2026-08-14-name-editor-v2'
+const SERVICE_WORKER_RELEASE = '2026-08-15-card-bubbles-v1'
 
 if ('serviceWorker' in navigator) {
   let reloadingForUpdate = false
